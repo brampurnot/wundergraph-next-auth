4789bdffcb6dcb3b2371213f7114164b1e4a3b32
brampurnot <bpurnot@icloud.com>
2024-05-29 14:18:32 +0200

    First commit

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,38 @@
+import NextAuth, { AuthOptions } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import KeycloakProvider from 'next-auth/providers/keycloak';
+
+import jose from 'jose';
+
+export const authOptions: AuthOptions = {
+	session: {
+		strategy: 'jwt',
+	},
+	callbacks: {
+		async jwt({ token }) {
+			return token;
+		},
+		async session({ session, token, user }) {
+			// Send properties to the client, like an access_token and user id from a provider.
+			return session;
+		},
+	},
+	// Configure one or more authentication providers
+	providers: [
+		GithubProvider({
+			clientId: process.env.GITHUB_ID,
+			clientSecret: process.env.GITHUB_SECRET,
+		}),
+		KeycloakProvider({
+			clientId: 'orchestr8-api',
+			clientSecret: 'bBRP2VD0xPvT2JYZFR6HUlZdSwpNj1HR',
+			issuer: 'https://keycloak.devadmin.orchestr8.cloud/realms/orchestrate',
+			id: 'keycloak',
+			name: 'keycloak',
+			jwks_endpoint:
+			  'https://keycloak.devadmin.orchestr8.cloud/realms/orchestrate/protocol/openid-connect/certs',
+		  }),
+		// ...add more providers here
+	],
+};
+export default NextAuth(authOptions);
